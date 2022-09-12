88b06785bdbcbd0e1ad2d4dfe614fefcdd734b20
Erik Erola <erik.erola@tuni.fi>
2022-09-12 10:11:26 +0300

    Exercises: variables

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -2,9 +2,8 @@
 // Make me compile!
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
+    let x: i32;
     x = 5;
     println!("x has the value {}", x);
 }
