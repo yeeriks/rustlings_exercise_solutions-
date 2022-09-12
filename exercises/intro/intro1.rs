8a6d208e217698868c2db8a7e4dbeceb5c129de5
Erik Erola <erik.erola@tuni.fi>
2022-09-12 09:54:50 +0300

    Excersises: Intro

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -9,7 +9,6 @@
 // when you change one of the lines below! Try adding a `println!` line, or try changing
 // what it outputs in your terminal. Try removing a semicolon and see what happens!
 
-// I AM NOT DONE
 
 fn main() {
     println!("Hello and");
@@ -19,6 +18,7 @@ fn main() {
     println!(r#" | '__| | | / __| __| | | '_ \ / _` / __| "#);
     println!(r#" | |  | |_| \__ \ |_| | | | | | (_| \__ \ "#);
     println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
+
     println!(r#"                               |___/      "#);
     println!();
     println!("This exercise compiles successfully. The remaining exercises contain a compiler");
